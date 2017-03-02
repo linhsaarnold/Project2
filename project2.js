f111f5643812dcd09a08f2a50f412055d218d0f1
linhsaarnold <linhsa.arnold@gmail.com>
2017-03-01 16:41:41 -0800

    Update project2.js
    added some functions for alerts and counting (counting isn't working... needs to be adjusted!!)

--- project2.js
+++ project2.js
@@ -1,3 +1,97 @@
-function navbarsearch(){
+function navbarsearch() {
     alert("I'm sorry, we can't find what you're looking for.");
+}
+
+function moreinfo() {
+    alert("No Info Available");
+}
+
+var countA = 0;
+function addinterestA() {
+    countA++;
+    document.getElementById("intA").innerHTML = countA;
+}
+
+var countB = 0;
+function addinterestA() {
+    countB++;
+    document.getElementById("intB").innerHTML = countB;
+}
+
+var countC = 0;
+function addinterestC() {
+    countC++;
+    document.getElementById("intC").innerHTML = countC;
+}
+
+var countD = 0;
+function addinterestD() {
+    countD++;
+    document.getElementById("intD").innerHTML = countD;
+}
+
+var countE = 0;
+function addinterestE() {
+    countE++;
+    document.getElementById("intE").innerHTML = countE;
+}
+
+var countF = 0;
+function addinterestF() {
+    countF++;
+    document.getElementById("intF").innerHTML = countF;
+}
+
+var countG = 0;
+function addinterestG() {
+    countG++;
+    document.getElementById("intG").innerHTML = countG;
+}
+
+var countH = 0;
+function addinterestH() {
+    countH++;
+    document.getElementById("intH").innerHTML = countH;
+}
+
+var countI = 0;
+function addinterestI() {
+    countI++;
+    document.getElementById("intI").innerHTML = countI;
+}
+
+var countJ = 0;
+function addinterestJ() {
+    countJ++;
+    document.getElementById("intJ").innerHTML = countJ;
+}
+
+var countK = 0;
+function addinterestK() {
+    countK++;
+    document.getElementById("intK").innerHTML = countK;
+}
+
+var countL = 0;
+function addinterestL() {
+    countL++;
+    document.getElementById("intL").innerHTML = countL;
+}
+
+var countM = 0;
+function addinterestM() {
+    countM++;
+    document.getElementById("intM").innerHTML = countM;
+}
+
+var countN = 0;
+function addinterestN() {
+    countN++;
+    document.getElementById("intN").innerHTML = countN;
+}
+
+var countO = 0;
+function addinterestO() {
+    countO++;
+    document.getElementById("intO").innerHTML = countO;
 }
